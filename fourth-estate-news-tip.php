01a920658ae946a6fc7ebfaad6b1b761f37ad00a
Zildjian Jax Olis <zildjianjaxolis@gmail.com>
2022-04-24 00:40:47 +0800

    - Update version number for cache busting

--- fourth-estate-news-tip.php
+++ fourth-estate-news-tip.php
@@ -35,7 +35,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'FOURTH_ESTATE_NEWS_TIP_VERSION', '1.0.2' );
+define( 'FOURTH_ESTATE_NEWS_TIP_VERSION', '1.0.3' );
 
 define( 'NEWS_TIP_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
 
